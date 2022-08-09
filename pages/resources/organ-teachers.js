d557bde14f2dee1dc8bc4d87839c92f00e968eab
Luke Graham <luke.evan.graham@gmail.com>
2022-08-08 22:43:36 -0700

    organ teachers and subs up

--- pages/resources/organ-teachers.js
+++ pages/resources/organ-teachers.js
@@ -1,17 +1,40 @@
 import Layout from "../../components/Layout/Layout";
 import { getGlobalData } from "../../lib/api";
+import { getOrganTeachers } from "../../lib/resources";
+
+import classes from "./organ-teachers.module.scss";
 
 export async function getStaticProps() {
-  const [globalData] = await Promise.all([getGlobalData()]);
+  const [globalData, teachersData] = await Promise.all([
+    getGlobalData(),
+    getOrganTeachers(),
+  ]);
   return {
-    props: { globalData },
+    props: { globalData, teachersData },
     revalidate: 1,
   };
 }
 
-const OrganTeachers = ({ globalData }) => (
+const OrganTeachers = ({ globalData, teachersData }) => (
   <Layout globalData={globalData}>
-    <div>Organ Teachers</div>
+    <div className="row">
+      <div>Organ Teachers</div>
+      {console.log("TD: ", teachersData)}
+      <div className={classes.Teachers}>
+        {teachersData.map((teacher) => (
+          <div key={teacher.id} className={classes.Teachers__Teacher}>
+            <div className={classes.Teachers__Teacher_name}>
+              {teacher.attributes.firstName} {teacher.attributes.lastName}
+            </div>
+            <div>{teacher.attributes.Phone}</div>
+            <a href={`mailto: ${teacher.attributes.Email}`}>
+              {teacher.attributes.Email}
+            </a>
+            {teacher.attributes.Website ? <div><a href={teacher.attributes.Website}>Website</a></div> : null}
+          </div>
+        ))}
+      </div>
+    </div>
   </Layout>
 );
 
--- pages/resources/substitute-organists.js
+++ pages/resources/substitute-organists.js
@@ -1,17 +1,76 @@
 import Layout from "../../components/Layout/Layout";
 import { getGlobalData } from "../../lib/api";
+import { getSubOrganists } from "../../lib/resources";
+
+import classes from "./substitute-organists.module.scss";
 
 export async function getStaticProps() {
-  const [globalData] = await Promise.all([getGlobalData()]);
+  const [globalData, subOrganistsData] = await Promise.all([
+    getGlobalData(),
+    getSubOrganists(),
+  ]);
   return {
-    props: { globalData },
+    props: { globalData, subOrganistsData },
     revalidate: 1,
   };
 }
 
-const SubstituteOrganists = ({ globalData }) => (
+const SubstituteOrganists = ({ globalData, subOrganistsData }) => (
   <Layout globalData={globalData}>
-    <div>Substitute Organists</div>
+    <div className="row">
+      <div>Substitute Organists</div>
+
+      <div>
+        <p>
+          <strong>
+            Those whose information states "only Weddings/Funerals" are not
+            available on Sundays
+          </strong>
+        </p>
+
+        <p>
+          Substitutes listed are members of the local organ guild. The ones who
+          hold AGO certificates have passed requirements of the national
+          American Guild of Organists for various levels of service playing and
+          choral conducting. However, the San Diego AGO is not an endorsement
+          agency for service provided by the persons listed here. To learn
+          proficiency of substitutes, feel free to ask them for references.
+        </p>
+      </div>
+
+      <div className={classes.Organists}>
+        {subOrganistsData.map((organist) => (
+          <div key={organist.id} className={classes.Organists__Organist}>
+            <div className={classes.Organists__Organist_name}>{`${
+              organist.attributes.Prefix ? organist.attributes.Prefix : ""
+            } ${organist.attributes.firstName} ${
+              organist.attributes.lastName
+            }`}</div>
+            <div>{organist.attributes.Phone}</div>
+            <a href={`mailto: ${organist.attributes.Email}`}>
+              {organist.attributes.Email}
+            </a>
+
+            <div className={classes.Organists__Organist_availability}>
+              Available as:
+            </div>
+            <ul>
+              {organist.attributes.liturgicalExperience ? (
+                <li>Liturgical Experience</li>
+              ) : null}
+              {organist.attributes.AGOCertifiedOrganist ? (
+                <li>AGO Certified Organist</li>
+              ) : null}
+              {organist.attributes.Pianist ? <li>Pianist</li> : null}
+              {organist.attributes.Vocalist ? <li>Vocalist</li> : null}
+              {organist.attributes.onlyWeddingsAndFunerals ? (
+                <li>Only weddings and funerals</li>
+              ) : null}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
   </Layout>
 );
 
