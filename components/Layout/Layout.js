501b08df4edc5cd526b4c2345a79dab5eb5bfc17
Cefalu Wave <luke.evan.graham@gmail.com>
2022-08-19 23:29:33 -0700

    mobile nav running

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,19 @@
 import { useState } from "react";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import MegaMenu from "../../components/Navigation/MegaMenu/MegaMenu";
 
 const Layout = ({ children, globalData }) => {
   const [megaMenuContent, setMegaMenuContent] = useState(false);
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
+  };
 
   let megaMenu = (children) => <MegaMenu content={children} />;
 
@@ -14,8 +23,16 @@ const Layout = ({ children, globalData }) => {
         globalData={globalData}
         setMegaMenuContent={setMegaMenuContent}
         megaMenuContent={megaMenuContent}
+        drawerToggleClicked={sideDrawerToggleHandler}
       />
       {megaMenuContent ? <MegaMenu content={megaMenuContent} /> : null}
+      <SideDrawer
+        open={showSideDrawer}
+        closed={sideDrawerClosedHandler}
+        globalData={globalData}
+        setMegaMenuContent={setMegaMenuContent}
+        megaMenuContent={megaMenuContent}
+      />
       <main>{children}</main>
     </>
   );
--- components/Navigation/Toolbar/Toolbar.js
+++ components/Navigation/Toolbar/Toolbar.js
@@ -1,19 +1,30 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 import classes from "./Toolbar.module.scss";
 
-const Toolbar = ({ globalData, setMegaMenuContent, megaMenuContent }) => (
+const Toolbar = ({
+  globalData,
+  setMegaMenuContent,
+  megaMenuContent,
+  drawerToggleClicked,
+}) => (
   <div className={classes.Toolbar}>
-    <div className={classes.Toolbar__Logo}>
-      {console.log("GD: ", globalData)}
-      <Image src={globalData.Navbar.logo.data.attributes.url} layout="fill" />
+    <Link href="/">
+      <div className={classes.Toolbar__Logo}>
+        <Image src={globalData.Navbar.logo.data.attributes.url} layout="fill" />
+      </div>
+    </Link>
+    <div className={classes.Toolbar__DesktopOnly}>
+      <NavigationItems
+        links={globalData.Navbar.links}
+        setMegaMenuContent={setMegaMenuContent}
+        megaMenuContent={megaMenuContent}
+      />
     </div>
-    <NavigationItems
-      links={globalData.Navbar.links}
-      setMegaMenuContent={setMegaMenuContent}
-      megaMenuContent={megaMenuContent}
-    />
+    <DrawerToggle clicked={drawerToggleClicked} />
   </div>
 );
 
