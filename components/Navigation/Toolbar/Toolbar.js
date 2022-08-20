78b2f1330679991a1374a11973e19a20bac0f03d
Cefalu Wave <luke.evan.graham@gmail.com>
2022-08-19 22:16:54 -0700

    adding logo

--- components/Navigation/Toolbar/Toolbar.js
+++ components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,14 @@
+import Image from "next/image"
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 import classes from "./Toolbar.module.scss";
 
 const Toolbar = ({ globalData, setMegaMenuContent, megaMenuContent }) => (
   <div className={classes.Toolbar}>
+    <div className={classes.Toolbar__Logo}>
+      {console.log("GD: ", globalData)}
+      <Image src={globalData.Navbar.logo.data.attributes.url} layout="fill" />
+    </div>
     <NavigationItems
       links={globalData.Navbar.links}
       setMegaMenuContent={setMegaMenuContent}
