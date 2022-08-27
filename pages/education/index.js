84010e0d0118f926c1957252f7f532ec73d5e753
Luke Graham <luke.evan.graham@gmail.com>
2022-08-26 18:33:11 -0700

    working on the education page

--- pages/education/index.js
+++ pages/education/index.js
@@ -1,6 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
 import Layout from "../../components/Layout/Layout";
 import { getGlobalData } from "../../lib/api";
 import { getEducationData } from "../../lib/education";
+
+import classes from "./index.module.scss";
 
 export async function getStaticProps() {
   const [globalData, educationData] = await Promise.all([
@@ -13,28 +17,98 @@ export async function getStaticProps() {
   };
 }
 
-const Education = ({ globalData, educationData }) => (
-  <Layout globalData={globalData}>
-    <div className="row">
-      <h1>Education</h1>
-      <section>
-        <h2>Scholarships</h2>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: educationData.ScholarshipsContent,
-          }}
-        ></div>
-      </section>
-      <section>
-        <h2>Certifications</h2>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: educationData.CertificationsContent,
-          }}
-        ></div>
-      </section>
-    </div>
-  </Layout>
-);
+const Education = ({ globalData, educationData }) => {
+  return (
+    <Layout globalData={globalData}>
+      <div className="row">
+        <h1>Education</h1>
+        {console.log("HERE: ", educationData)}
+        <section className={classes.Welcome}>
+          <div
+            dangerouslySetInnerHTML={{ __html: educationData.topText }}
+          ></div>
+          <div className={classes.Welcome__Image}>
+            <Image
+              src={educationData.topImage.data.attributes.url}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </section>
+      </div>
+      <div className={classes.WhiteBg}>
+        <div className="row u-padding-top-medium u-padding-bottom-medium">
+          <section className={classes.Scholarships}>
+            <h2>Scholarships</h2>
+            <h3>AGO/Spreckels Scholarship Auditions</h3>
+            <div className={classes.Scholarships__Main}>
+              <div
+                className={classes.Scholarships__Main__Body}
+                dangerouslySetInnerHTML={{
+                  __html: educationData.ScholarshipsContent,
+                }}
+              />
+              <div className={classes.Scholarships__Main__CTA}>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: educationData.ScholarshipsCTA,
+                  }}
+                />
+                <div className={classes.Scholarships__Main__CTA__docs}>
+                  {educationData.ScholarshipsDocuments.data.map((doc) => (
+                    <a href={doc.attributes.url} target="_blank" key={doc.id}>
+                      <div
+                        className={classes.Scholarships__Main__CTA__docs_doc}
+                      >
+                        <svg>
+                          <use xlinkHref="../images/sprite.svg#icon-file-pdf"></use>
+                        </svg>
+                        <div>{doc.attributes.name.split(".pdf")[0]}</div>
+                      </div>
+                    </a>
+                  ))}
+                  <a
+                    href={educationData.ScholarshipsApplication}
+                    target="_blank"
+                  >
+                    <div className={classes.Scholarships__Main__CTA__docs_doc}>
+                      <svg>
+                        <use xlinkHref="../images/sprite.svg#icon-pen"></use>
+                      </svg>
+                      <div>Application</div>
+                    </div>
+                  </a>
+                  <Link href="/">
+                    <a>
+                      <div
+                        className={classes.Scholarships__Main__CTA__docs_doc}
+                      >
+                        <svg>
+                          <use xlinkHref="../images/sprite.svg#icon-currency-dollar"></use>
+                        </svg>
+                        <div>Fee</div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div className="row">
+        <section>
+          <h2>Certifications</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: educationData.CertificationsContent,
+            }}
+          />
+        </section>
+      </div>
+    </Layout>
+  );
+};
 
 export default Education;
