720357b0c4119b42ec214b531d5883c1030b02d3
Luke Graham <luke.evan.graham@gmail.com>
2022-08-27 22:38:32 -0700

    social in footer

--- components/Layout/Footer/Footer.js
+++ components/Layout/Footer/Footer.js
@@ -1,17 +1,38 @@
-import Link from "next/link"
-import classes from "./Footer.module.scss"
+import Link from "next/link";
+import classes from "./Footer.module.scss";
 
 const Footer = () => (
-    <div className={classes.Footer}>
-        <div>American Guild of Organists, San Diego Chapter</div>
-        <div className={classes.Footer__UsefulLinks}>
-            <div>Useful Links</div>
-            <ul>
-                <li> <Link href="/join"><a> &rsaquo; Join</a></Link></li>
-                <li> <Link href="/resources/area-organs"><a> &rsaquo; Area Organs</a></Link></li>
-            </ul>
-        </div>
+  <div className={classes.Footer}>
+    <div>American Guild of Organists, San Diego Chapter</div>
+    <div className={classes.Footer__UsefulLinks}>
+      <div>Useful Links</div>
+      <ul>
+        <li>
+          {" "}
+          <Link href="/join">
+            <a> &rsaquo; Join</a>
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link href="/resources/area-organs">
+            <a> &rsaquo; Area Organs</a>
+          </Link>
+        </li>
+      </ul>
     </div>
-)
+    <div className={classes.Footer__Social}>
+      <a
+        href="https://www.facebook.com/AGOSanDiego"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg>
+          <use xlinkHref="../images/sprite.svg#icon-facebook"></use>
+        </svg>
+      </a>
+    </div>
+  </div>
+);
 
 export default Footer;
