8c8a5b420e8a02ec5d750d2b9e0fa3b5dbdb7648
Luke Graham <luke.evan.graham@gmail.com>
2022-09-18 14:15:54 -0700

    purchase application is working with sandbox

--- components/Paypal/PaypalCheckoutButton.js
+++ components/Paypal/PaypalCheckoutButton.js
@@ -3,6 +3,45 @@ import { PayPalButtons } from "@paypal/react-paypal-js";
 
 const PaypalCheckoutButton = ({ product }) => {
   const [paidFor, setPaidFor] = useState(false);
+  const [error, setError] = useState(null);
+
+  let checkout = (
+    <PayPalButtons
+      style={{
+        height: 48,
+        tagline: false,
+        shape: "pill",
+      }}
+      createOrder={(data, actions) => {
+        return actions.order
+          .create({
+            purchase_units: [
+              {
+                description: product.description,
+                amount: {
+                  currency_code: "USD",
+                  value: product.price,
+                },
+              },
+            ],
+          })
+          .then((orderId) => {
+            // Your code here after create the order
+            return orderId;
+          });
+      }}
+      onApprove={async (data, actions) => {
+        const order = await actions.order.capture();
+        // console.log("order", order);
+
+        handleApprove(data.orderID);
+      }}
+      onError={(err) => {
+        setError(err);
+        console.error("PayPal Checkout onError", err);
+      }}
+    />
+  );
 
   const handleApprove = (orderId) => {
     // Call backend function to fulfill order
@@ -17,44 +56,15 @@ const PaypalCheckoutButton = ({ product }) => {
 
   if (paidFor) {
     // Display success message, modal or redirect user to success page
-    alert("Thank you for your purchase!");
+    // alert("Thank you for your purchase!");
+    checkout = <div>Thank you for your purchase!</div>;
   }
 
-  return (
-    <>
-      <PayPalButtons
-        style={{
-          height: 48,
-          tagline: false,
-          shape: "pill",
-        }}
-        createOrder={(data, actions) => {
-          return actions.order
-            .create({
-              purchase_units: [
-                {
-                  description: product.description,
-                  amount: {
-                    currency_code: "USD",
-                    value: product.price,
-                  },
-                },
-              ],
-            })
-            .then((orderId) => {
-              // Your code here after create the order
-              return orderId;
-            });
-        }}
-        onApprove={async (data, actions) => {
-          const order = await actions.order.capture();
-          console.log("order", order);
+  if (error) {
+    checkout = <div>{error}</div>;
+  }
 
-          handleApprove(data.orderID);
-        }}
-      />
-    </>
-  );
+  return <>{checkout}</>;
 };
 
 export default PaypalCheckoutButton;
