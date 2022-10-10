7391e4e9f95976e6d5ea6753c6a592a0352dd004
Luke Graham <luke.evan.graham@gmail.com>
2022-10-09 22:18:28 -0700

    search function up and running

--- components/Navigation/SideDrawer/SideDrawer.js
+++ components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,9 @@
+import { useState, useRef } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import Image from "next/image";
 import Backdrop from "../../UI/Backdrop/Backdrop";
+import Brandname from "../../UI/Brandname/Brandname";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import MegaMenu from "../MegaMenu/MegaMenu";
 
@@ -12,35 +15,75 @@ const SideDrawer = ({
   globalData,
   setMegaMenuContent,
   megaMenuContent,
-}) => (
-  <>
-    <Backdrop show={open} clicked={closed} />
-    <div
-      className={
-        open
-          ? `${classes.SideDrawer} ${classes.SideDrawer__open}`
-          : `${classes.SideDrawer} ${classes.SideDrawer__closed}`
-      }
-    >
-      <Link href="/">
-        <a>
-          <div className={classes.SideDrawer__logo}>
-            <Image
-              src={globalData.Navbar.logo.data.attributes.url}
-              layout="fill"
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef();
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push({
+      pathname: "/search",
+      query: { keyword: searchTerm },
+    });
+    closed()
+  };
+
+  return (
+    <>
+      <Backdrop show={open} clicked={closed} />
+      <div
+        className={
+          open
+            ? `${classes.SideDrawer} ${classes.SideDrawer__open}`
+            : `${classes.SideDrawer} ${classes.SideDrawer__closed}`
+        }
+      >
+        <Link href="/">
+          <a className={classes.SideDrawer__Brand}>
+            <div className={classes.SideDrawer__Brand__logo}>
+              <Image
+                src={globalData.Navbar.logo.data.attributes.url}
+                layout="fill"
+              />
+            </div>
+            <Brandname />
+          </a>
+        </Link>
+        <div className={classes.SideDrawer__search}>
+          <form className={classes.SideDrawer__search__Form} onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="searchTerm"
+              id="searchTerm"
+              placeholder="Search"
+              autoFocus
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              ref={inputRef}
             />
-          </div>
-        </a>
-      </Link>
-      <NavigationItems
-        links={globalData.Navbar.links}
-        setMegaMenuContent={setMegaMenuContent}
-        megaMenuContent={megaMenuContent}
-        fromSideDrawer
-      />
-      {megaMenuContent ? <MegaMenu content={megaMenuContent} /> : null}
-    </div>
-  </>
-);
+            {/* <Link href={{ pathname: "/search", query: { keyword: searchTerm } }}> */}
+            <div
+              className={classes.SideDrawer__search__Form_SearchButton}
+              onClick={handleSubmit}
+            >
+              <svg>
+                <use xlinkHref="/images/sprite.svg#icon-magnifying-glass"></use>
+              </svg>
+            </div>
+            {/* </Link> */}
+          </form>
+        </div>
+        <NavigationItems
+          links={globalData.Navbar.links}
+          setMegaMenuContent={setMegaMenuContent}
+          megaMenuContent={megaMenuContent}
+          fromSideDrawer
+        />
+        {megaMenuContent ? <MegaMenu content={megaMenuContent} /> : null}
+      </div>
+    </>
+  );
+};
 
 export default SideDrawer;
