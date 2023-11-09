103ce10882380d900ff355fc8f8ad2dad117e6ef
Luke Graham <luke.evan.graham@gmail.com>
2023-11-08 22:53:55 -0800

    switching to sparkpost

--- pages/api/boardEmail.js
+++ pages/api/boardEmail.js
@@ -1,37 +1,85 @@
-const nodemailer = require("nodemailer");
+import axios from "axios";
 
 export default function handler(req, res) {
-  res.status(200).json({ status: 200 });
-
-  async function main() {
-    let transporter = nodemailer.createTransport({
-      host: "mail.gandi.net",
-      port: "465",
-      secure: "true",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
+  let data = JSON.stringify({
+    recipients: [
+      {
+        address: req.body.recipient,
       },
-    });
-
-    let info = await transporter.sendMail({
-      from: '"AGOSD.ORG" <donotreply@agosd.org>',
-      // to: "luke@grahamwebworks.com",
-      to: req.body.recipient,
+      {
+        address: "luke@grahamwebworks.com",
+      },
+    ],
+    content: {
+      from: {
+        email: "luke@mail.grahamwebworks.com",
+        name: "AGOSD.ORG",
+      },
       subject: "Email from agosd.org",
       text: `${req.body.name} (${req.body.email}) just sent this message via agosd.org:
-            
+
             ${req.body.body}`,
-    });
+    },
+  });
 
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Server is ready to take our messages", success);
-      }
-    });
-  }
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://api.sparkpost.com/api/v1/transmissions",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: process.env.SPARKPOST_API_KEY,
+    },
+    data: data,
+  };
 
-  main().catch(console.error);
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      res.status(200).json({ status: 200 });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
+
+// NODEMAILER CODE
+
+// const nodemailer = require("nodemailer");
+
+// export default function handler(req, res) {
+//   res.status(200).json({ status: 200 });
+
+//   async function main() {
+//     let transporter = nodemailer.createTransport({
+//       host: "mail.gandi.net",
+//       port: "465",
+//       secure: "true",
+//       auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     let info = await transporter.sendMail({
+//       from: '"AGOSD.ORG" <donotreply@agosd.org>',
+//       // to: "luke@grahamwebworks.com",
+//       to: req.body.recipient,
+//       subject: "Email from agosd.org",
+//       text: `${req.body.name} (${req.body.email}) just sent this message via agosd.org:
+
+//             ${req.body.body}`,
+//     });
+
+//     transporter.verify(function (error, success) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log("Server is ready to take our messages", success);
+//       }
+//     });
+//   }
+
+//   main().catch(console.error);
+// }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,36 +1,80 @@
-const nodemailer = require("nodemailer");
+import axios from "axios";
 
 export default function handler(req, res) {
-  res.status(200).json({ status: 200 });
-
-  async function main() {
-    let transporter = nodemailer.createTransport({
-      host: "mail.gandi.net",
-      port: "465",
-      secure: "true",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
+  let data = JSON.stringify({
+    recipients: [
+      {
+        address: "info@agosd.org",
       },
-    });
-
-    let info = await transporter.sendMail({
-      from: '"AGOSD.ORG" <donotreply@agosd.org',
-      to: "info@agosd.org",
+      { address: "luke@grahamwebworks.com" },
+    ],
+    content: {
+      from: {
+        email: "luke@mail.grahamwebworks.com",
+        name: "AGOSD.ORG",
+      },
       subject: "Contact form message from AGOSD.ORG",
       text: `${req.body.name} (${req.body.email}) just sent this message via AGOSD.ORG's contact form:
-        
+
         ${req.body.message}`,
-    });
+    },
+  });
 
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Server is ready to take our message", success);
-      }
-    });
-  }
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://api.sparkpost.com/api/v1/transmissions",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: process.env.SPARKPOST_API_KEY,
+    },
+    data: data,
+  };
 
-  main().catch(console.error);
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      res.status(200).json({ status: 200 });
+    })
+    .catch((error) => console.log(error));
 }
+
+// NODEMAILER CODE
+
+// const nodemailer = require("nodemailer");
+
+// export default function handler(req, res) {
+//   res.status(200).json({ status: 200 });
+
+//   async function main() {
+//     let transporter = nodemailer.createTransport({
+//       host: "mail.gandi.net",
+//       port: "465",
+//       secure: "true",
+//       auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     let info = await transporter.sendMail({
+//       from: '"AGOSD.ORG" <donotreply@agosd.org',
+//       to: "info@agosd.org",
+//       subject: "Contact form message from AGOSD.ORG",
+//       text: `${req.body.name} (${req.body.email}) just sent this message via AGOSD.ORG's contact form:
+
+//         ${req.body.message}`,
+//     });
+
+//     transporter.verify(function (error, success) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log("Server is ready to take our message", success);
+//       }
+//     });
+//   }
+
+//   main().catch(console.error);
+// }
